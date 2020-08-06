f0d188982ed38f2f1ce63f94611ebabc049604f6
claire froelich <claire.froelich@gmail.com>
2020-08-05 17:47:31 -0700

    hook up contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,28 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+})
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-postcss`],
+  plugins: [
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `to8vy027sqma`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.GATSBY_CONTENTFUL_DELIVERY_KEY,
+      },
+    },
+  ],
 }
